65724934793219d1aa1653da08889fd20201e665
unn4m3d <smelnikov871@gmail.com>
2015-12-18 19:52:22 +1000

    Added lanterna submodule

---
+++ src/io/github/unn4m3d/xmml/Launcher.java
@@ -0,0 +1,12 @@
+package io.github.unn4m3d.xmml;
+
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//TODO
+	}
+
+}
---
+++ src/io/github/unn4m3d/xmml/MainClass.java
@@ -0,0 +1,48 @@
+package io.github.unn4m3d.xmml;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import net.launcher.*;
+import net.launcher.utils.*;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+
+			String jarpath = MainClass.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+			int memory = (int)Config.get("memory", 1024);
+			
+			ArrayList<String> params = new ArrayList<String>();
+           
+			params.add("java");
+			params.add("-Xmx"+memory+"m");
+			params.add("-Xms"+memory+"m");
+			params.add("-XX:MaxPermSize=128m");
+			params.add("-Dfile.encoding=UTF-8");
+			if(System.getProperty("os.name").toLowerCase().startsWith("mac"))
+			{
+				params.add("-Xdock:name=Minecraft");
+				params.add("-Xdock:icon="+ClientUtils.getAssetsDir().toString()+"/favicon.png");
+			}
+			params.add("-classpath");
+			params.add(jarpath);
+			params.add(Launcher.class.getCanonicalName());
+
+			ProcessBuilder pb = new ProcessBuilder(params);
+			pb.directory(new File(ClientUtils.getAssetsDir().toString()));
+			Process process = pb.start();
+			if (process == null) throw new Exception("Launcher can't be started!");
+			System.exit(0);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
